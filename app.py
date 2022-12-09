c98481f6591969c66c7ac466cea356ec29b20835
Michael Renehan <michaelrenehan@Michaels-iMac.local>
2022-12-09 11:42:05 -0500

    initial app

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import torch, detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+#from google.colab.patches import cv2_imshow
+
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
